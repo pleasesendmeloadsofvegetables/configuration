ccd3f79a5b940e39b4bc8e13486431e7da20871e
burger <(none)>
2024-12-12 22:36:13 -0800

    add services

--- guix/home-environment/user.scm
+++ guix/home-environment/user.scm
@@ -5,19 +5,20 @@
   #:use-module (packages hy3)
   ;; GNU Services
   #:use-module (gnu services)
-  #:use-module (gnu services shepherd)
-
   ;; GNU Home
   #:use-module (gnu home)
   #:use-module (gnu home services shells)
   #:use-module (gnu home services dotfiles)
   #:use-module (gnu home services sound)
+  #:use-module (gnu home services desktop)
+  #:use-module (gnu home services shepherd)
   ;; GNU Packages
   #:use-module (gnu packages version-control)
   #:use-module (gnu packages curl)
   #:use-module (gnu packages wm)
   #:use-module (gnu packages xdisorg)
   #:use-module (gnu packages terminals)
+  #:use-module (gnu packages linux)
   ;; Non-GNU Packages
   #:use-module (nongnu packages nvidia)
   #:use-module (nongnu packages mozilla)
@@ -37,6 +38,8 @@
   (services
    (list (service home-channels-config-service-type)
          (service home-bash-service-type)
+
+         (service home-dbus-service-type)
 
          (service home-pipewire-service-type
                   (home-pipewire-configuration (wireplumber
